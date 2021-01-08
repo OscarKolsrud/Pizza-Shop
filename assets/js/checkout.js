bb1c72cb86bc9eb4727a2101c10abeba82237516
Oscar Kolsrud <30559109+OscarKolsrud@users.noreply.github.com>
2021-01-08 11:47:44 +0100

    Update checkout.js

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -204,8 +204,30 @@ form.onsubmit = function (evt) {
                     "<h4 class='text-center'>Ordre ref.: " + docRef.id + "</h4>" +
                     "<br>" +
                     "<div class='col text-center'>" +
+                    "<div id='ytplayer'></div>" +
+                    "<br>" +
                     "<button class='btn btn-primary btn-sm mt-3' type='button' onclick='closeCheckout(true);'>Lukk</button>" +
                     "</div>";
+            
+                  //YT Player start
+                  var tag = document.createElement('script');
+                  tag.src = "https://www.youtube.com/player_api";
+                  var firstScriptTag = document.getElementsByTagName('script')[0];
+                  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                  // Replace the 'ytplayer' element with an <iframe> and
+                  // YouTube player after the API code downloads.
+                  var player;
+                  function onYouTubePlayerAPIReady() {
+                    player = new YT.Player('ytplayer', {
+                      height: '315',
+                      width: '560',
+                      videoId: 'CwRttSfnfcc',
+                      autoplay: 1,
+                      loop: 1
+                    });
+                  }
+            
                 //Clear the cart and "reset"
                 sessionStorage.balance = 100000;
                 saveCart([]);
